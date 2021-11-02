fee9ff41c6763189f1ddc2538bd30d01af84598b
Weiye Zhang <84930696+Hydeartorias@users.noreply.github.com>
2021-11-02 15:29:38 -0500

    Add files via upload

---
+++ db.js
@@ -0,0 +1,47 @@
+let COMP1010 = {code:"COMP1010", name:"COMP SCI I", grade:"0.0", color:"BLUE", tasts:"0"};
+let COMP1020 = {code:"COMP1020", name:"COMP SCI II", grade:"0.0", color:"BLUE", tasts:"0"};
+let COMP2080 = {code:"COMP2080", name:"ANALYSIS/ALG", grade:"0.0", color:"BLUE", tasts:"0"};
+let COMP2140 = {code:"COMP2140", name:"DATA STR ALG", grade:"0.0", color:"BLUE", tasts:"0"};
+let COMP2150 = {code:"COMP2150", name:"OBJECT ORIENT", grade:"0.0", color:"BLUE", tasts:"0"};
+let COMP2160 = {code:"COMP2160", name:"PROG PRACTICES", grade:"0.0", color:"BLUE", tasts:"0"};
+let COMP3020 = {code:"COMP3020", name:"HUMAN COM INT1", grade:"0.0", color:"BLUE", tasts:"0"};
+let COMP3350 = {code:"COMP3350", name:"SOFTWARE ENG I", grade:"0.0", color:"BLUE", tasts:"0"};
+let COMP3380 = {code:"COMP3380", name:"DATABASES", grade:"0.0", color:"BLUE", tasts:"0"};
+let COMP3430 = {code:"COMP3430", name:"OPERATING SYS", grade:"0.0", color:"BLUE", tasts:"0"};
+let COMP4620 = {code:"COMP4620", name:"PROF PRACTICES IN COMP SCI", grade:"0.0", color:"BLUE", tasts:"0"};
+
+let MATH1010 = {code:"MATH1010", name:"APP FIN MATH", grade:"0.0", color:"GREEN", tasts:"0"};
+let MATH1020 = {code:"MATH1020", name:"MATH IN ART", grade:"0.0", color:"GREEN", tasts:"0"};
+let MATH1220 = {code:"MATH1220", name:"LINEAR ALGEBRA 1", grade:"0.0", color:"GREEN", tasts:"0"};
+let MATH1300 = {code:"MATH1300", name:"VEC GEO LIN ALG", grade:"0.0", color:"GREEN", tasts:"0"};
+let MATH2040 = {code:"MATH2040", name:"CURVES AND SURFACES", grade:"0.0", color:"GREEN", tasts:"0"};
+let MATH2180 = {code:"MATH2180", name:"REAL ANALYSIS 1", grade:"0.0", color:"GREEN", tasts:"0"};
+let MATH2720 = {code:"MATH2720", name:"MULTIVAR CALC", grade:"0.0", color:"GREEN", tasts:"0"};
+let MATH3340 = {code:"MATH3340", name:"COMPLEX ANALYSIS 1", grade:"0.0", color:"GREEN", tasts:"0"};
+let MATH3490 = {code:"MATH3490", name:"OPTIMIZATION", grade:"0.0", color:"GREEN", tasts:"0"};
+let MATH4270 = {code:"MATH4270", name:"ALGEBRAIC TOPOLOGY", grade:"0.0", color:"GREEN", tasts:"0"};
+let MATH4450 = {code:"MATH4450", name:"NUMBER THEORY 2", grade:"0.0", color:"GREEN", tasts:"0"};
+
+let BIOL1000 = {code:"BIOL1000", name:"FOUNDATIONS", grade:"0.0", color:"YELLOW", tasts:"0"};
+let BIOL1010 = {code:"BIOL1010", name:"DIVERSE & INTER", grade:"0.0", color:"YELLOW", tasts:"0"};
+let BIOL1340 = {code:"BIOL1340", name:"EARTH ENVIRONMT/ALG", grade:"0.0", color:"YELLOW", tasts:"0"};
+let BIOL2210 = {code:"BIOL2210", name:"THE CHORDATES", grade:"0.0", color:"YELLOW", tasts:"0"};
+let BIOL2500 = {code:"BIOL2500", name:"GENETICS 1", grade:"0.0", color:"YELLOW", tasts:"0"};
+let BIOL3200 = {code:"BIOL3200", name:"ADV INVERTEBRATE BIOLOGY", grade:"0.0", color:"YELLOW", tasts:"0"};
+let BIOL3340 = {code:"BIOL3340", name:"BIOL OF PRIMITIVE FUNGY & ALLI", grade:"0.0", color:"YELLOW", tasts:"0"};
+let BIOL3500 = {code:"BIOL3500", name:"GENETICS 2", grade:"0.0", color:"YELLOW", tasts:"0"};
+let BIOL3600 = {code:"BIOL3600", name:"BIODIVERSITY & SUSTAINABILITY", grade:"0.0", color:"YELLOW", tasts:"0"};
+let BIOL4100 = {code:"BIOL4100", name:"HONOURS THESIS", grade:"0.0", color:"YELLOW", tasts:"0"};
+let BIOL4310 = {code:"BIOL4310", name:"APP POPULATION ECOLOGY", grade:"0.0", color:"YELLOW", tasts:"0"};
+
+let ENG1430 = {code:"ENG1430", name:"DESIGN ENGINEER", grade:"0.0", color:"RED", tasts:"0"};
+let ENG1440 = {code:"ENG1440", name:"INTRO STATICS", grade:"0.0", color:"RED", tasts:"0"};
+let ENG2040 = {code:"ENG2040", name:"ENG COMM: STRAT,PRAC,DESIGN", grade:"0.0", color:"RED", tasts:"0"};
+let ENG3000 = {code:"ENG3000", name:"ENGINEERING ECONOMICS", grade:"0.0", color:"RED", tasts:"0"};
+let ENG3020 = {code:"ENG3020", name:"TECH & SOCIETY", grade:"0.0", color:"RED", tasts:"0"};
+let ENG4020 = {code:"ENG4020", name:"PROF ENG PRACT IN MB", grade:"0.0", color:"RED", tasts:"0"};
+let ENG4100 = {code:"ENG4100", name:"CONTEMP TOPICS IN ENGG PRAC", grade:"0.0", color:"RED", tasts:"0"};
+let ENG4110 = {code:"ENG4110", name:"OPERATIONAL EXCELLENCE", grade:"0.0", color:"RED", tasts:"0"};
+let ENG4800 = {code:"ENG4800", name:"CO-OPERATIVE WORK 1", grade:"0.0", color:"RED", tasts:"0"};
+let ENG4810 = {code:"ENG4810", name:"CO-OPERATIVE WORK 2", grade:"0.0", color:"RED", tasts:"0"};
+let ENG4820 = {code:"ENG4820", name:"CO-OPERATIVE WORK 3", grade:"0.0", color:"RED", tasts:"0"};
